3818451f1bd8e5e57c3432dddf79d49b29720712
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-27 23:58:38 -0300

    📗  class 02 - config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+import Routes from './routes';
 
 export default function App() {
-  return <h1>Hmm ... Hellow ?</h1>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
