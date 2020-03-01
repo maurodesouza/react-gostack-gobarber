9e5d5d9c4865b865d094470c8c801d2637bfabe2
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-29 23:15:32 -0300

    📒  class 19 - add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/svgs/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard"> Dashboard </Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong> Mauro de Souza </strong>
+              <Link to="/profile"> Meu perfil </Link>
+            </div>
+            <img
+              src="https://randomuser.me/api/portraits/thumb/men/75.jpg"
+              alt="Mauro de Souza"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
